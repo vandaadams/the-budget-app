47e6b1904eb2ce9883b54e48753ecd2c0d37471a
Vanda Adams <vandamadams@gmail.com>
2020-04-09 12:39:38 +0100

    event listener setup for when user adds input

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-// 1. add event handler when user adds income/expenses
 // 2. get input value
 // 3. add new item to data structure
 // 4. display new item
 // 5. calculate budget
 // 6. display budget
 
+// BUDGET CONTROLLER
 const budgetController = (() => {
 
-  var x = 23;
-  var add = (a) => {
-    return x + a;
-  }
-
-  return {
-    publicTest: (b) => {
-      return add(b);
-    }
-  }
+ // code goes here
 
 })();
 
+// UI CONTROLLER
 const UIController = (() => {
 
+  // code goes here
+
 })();
 
+// GLOBAL CONTROLLER
 const controller = ((budgetCtrl, UICtrl) => {
 
-  var z= budgetCtrl.publicTest(5);
-
-  return {
-    anotherPublic: () => {
-      console.log(z);
-    }
+  const ctrlAddItem = () => {
+    console.log('it works')
   }
+
+  // 1. add event handler when user adds income/expenses
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', (event) => {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
 
 })(budgetController, UIController);
