8bcbabe9864fd5083e271b03bc24fe685fa2c9f6
Vanda Adams <vandamadams@gmail.com>
2020-04-15 19:01:11 +0100

    added function for displaying items

--- app.js
+++ app.js
@@ -65,7 +65,9 @@ var UIController = function() {
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputBtn: '.add__btn'
+    inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list'
   }
 
   return {
@@ -76,6 +78,26 @@ var UIController = function() {
         value: document.querySelector(DOMstrings.inputValue).value
       }
     },
+
+    addListItem: function(obj, type) {
+      var html, newHtml;
+
+      if (type === 'inc') {
+        element = DOMstrings.incomeContainer;
+
+        html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>'
+      } else if (type === 'exp') {
+        element = DOMstrings.expensesContainer;
+
+        html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>'
+      }
+
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value);
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -101,7 +123,10 @@ var controller = function(budgetCtrl, UICtrl) {
     var input, newItem;
 
     input = UICtrl.getInput();
+
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
+    UICtrl.addListItem(newItem, input.type);
   };
 
   return {
