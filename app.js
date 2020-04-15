a944d347058950aee1609aabf7e9c3800b5fcb9f
Vanda Adams <vandamadams@gmail.com>
2020-04-15 13:02:51 +0100

    controller setup update

--- app.js
+++ app.js
@@ -7,31 +7,81 @@
 // BUDGET CONTROLLER
 const budgetController = (() => {
 
- // code goes here
+  const Expense = (id, description, value) => {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
 
+  const Income = (id, description, value) => {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  const data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals : {
+      exp: 0,
+      inc: 0
+    }
+  }
 })();
 
 // UI CONTROLLER
 const UIController = (() => {
 
-  // code goes here
+  const DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
 
+  return {
+    getInput: () => {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      }
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  }
 })();
 
 // GLOBAL CONTROLLER
 const controller = ((budgetCtrl, UICtrl) => {
 
+  const setupEventListeners = () => {
+    const DOM = UICtrl.getDOMstrings();
+    // 1. add event handler when user adds income/expenses
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', (event) => {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
   const ctrlAddItem = () => {
-    console.log('it works')
-  }
+    const input = UICtrl.getInput();
+    console.log(input);
+  };
 
-  // 1. add event handler when user adds income/expenses
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', (event) => {
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
+  return {
+    init: () => {
+      console.log('application has started')
+      setupEventListeners();
     }
-  });
+  };
 
 })(budgetController, UIController);
+
+controller.init();
