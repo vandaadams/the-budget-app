a76dfa001a2e9d61c9d5d943769518efdd1d69ef
Vanda Adams <vandamadams@gmail.com>
2020-04-17 10:58:52 +0100

    added budget display function

--- app.js
+++ app.js
@@ -100,7 +100,11 @@ var UIController = function() {
     inputValue: '.add__value',
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
-    expensesContainer: '.expenses__list'
+    expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expensesLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
   }
 
   return {
@@ -143,6 +147,19 @@ var UIController = function() {
       fieldsArr[0].focus();
     },
 
+    displayBudget: function(obj) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+      if (obj.percentage > 0) {
+        document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+      }
+
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -170,7 +187,7 @@ var controller = function(budgetCtrl, UICtrl) {
     // returns budget
     var budget = budgetCtrl.getBudget();
     // displays budget
-    console.log(budget);
+    UICtrl.displayBudget(budget);
   };
 
   var ctrlAddItem = function() {
@@ -192,7 +209,13 @@ var controller = function(budgetCtrl, UICtrl) {
 
   return {
     init: function() {
-      console.log('application has started')
+      console.log('application has started');
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListeners();
     }
   };
