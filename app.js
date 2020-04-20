d7e66c45771c4e32b25787532797686c7c972fa5
Vanda Adams <vandamadams@gmail.com>
2020-04-20 12:26:08 +0100

    date display

--- app.js
+++ app.js
@@ -150,7 +150,8 @@ var UIController = function() {
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
     container: '.container',
-    expensesPercLabel: '.item__percentage'
+    expensesPercLabel: '.item__percentage',
+    dateLabel: '.budget__title--month'
   };
 
   var formatNumber =  function(num, type) {
@@ -248,6 +249,17 @@ var UIController = function() {
       });
     },
 
+    displayMonth: function() {
+      var now, month, year;
+
+      now = new Date();
+
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+      month = now.getMonth();
+      year = now.getFullYear();
+      document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -333,6 +345,7 @@ var controller = function(budgetCtrl, UICtrl) {
   return {
     init: function() {
       console.log('application has started');
+      UICtrl.displayMonth();
       UICtrl.displayBudget({
         budget: 0,
         totalInc: 0,
